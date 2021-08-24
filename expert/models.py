f34bab82d4cdd61e7094f6ff963f0eb3e64e9a18
Aleksandr Korovkin <teamer777@gmail.com>
2021-08-24 11:46:33 +0400

    test migrations

--- expert/models.py
+++ expert/models.py
@@ -5,6 +5,7 @@ class Expert(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     bio = models.TextField()
+    personal_life = models.TextField()
 
     def __str__(self) -> str:
         return f"{self.first_name} {self.last_name}"
