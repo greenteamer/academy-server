8bd6321e6c74bd54189a4e377ad129ab87aff412
Aleksandr Korovkin <teamer777@gmail.com>
2021-08-25 14:38:56 +0400

    add static

--- deploy.sh
+++ deploy.sh
@@ -5,3 +5,4 @@ ENV_DIR=$(dirname $APP_DIR)
 # python_env="/.env/bin/python3"
 $ENV_DIR/.env/bin/python3 -m pip install -r deps.pip
 $ENV_DIR/.env/bin/python3 $APP_DIR/manage.py migrate
+$ENV_DIR/.env/bin/python3 $APP_DIR/manage.py collectstatic
--- project/settings.py
+++ project/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["212.193.49.101"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "212.193.49.101"]
 
 
 # Application definition
@@ -120,6 +120,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+print(STATIC_ROOT)
+
+STATICFILES_DIRS = (
+    # os.path.join(BASE_DIR, "/static"),
+    # os.path.join(BASE_DIR, "/media"),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
